35981e110574986f6549e4f02000f80eb5751e2b
=Alexh <=>
2017-10-26 20:35:37 +0200

    refactor index.php, move logic to  indexController

--- index.php
+++ index.php
@@ -6,88 +6,22 @@ session_start();
 require_once 'view/searchInput.html';
 require_once 'model/Domain.php';
 require_once 'model/inc/db.inc.php';
+require_once 'controller/IndexController.php';
 
-// /https?:\/\/w{3}\.[a-z]+\.[a-z]{2,3}/
-// https?:\/\/w{3}\.[a-z]+\.[a-z]+\/?([a-z]+)?
-// https?:\/\/w{3}\.[a-z]+\.[a-z]+(\/?([a-z]+)?)+(\.html)?
 
 // Create db connection
 Domain::connectToDb($db);
 
-// Create domain and get all searched
+// Create IndexController
+$indexController = new IndexController();
+
+// Create domain and get all found
 $domain = new Domain();
 $domains = $domain->getAllDomains();
 
 // Flag for page reload
 $reloaded = false;
 
-/**
- * @param string $urlSearchThrough
- * the given url (Browse the URL)
- *
- * @return array
- * array with search results (links found)
- *
- */
-function searchWebLinkInUrl(string $urlSearchThrough) : array
-{
-    // Retrieve the entire content(html) from the specified domain
-    $url = 'http://' . $urlSearchThrough;
-    $theHtmlToParse = file_get_contents($url);
-
-    // Filter all links from the content
-    $pattern = '/https?:\/\/w{3}\.[a-z]+\.[a-z]+(\/?([a-z]+)?)+(\.html)?/';
-    preg_match_all($pattern, $theHtmlToParse, $ausgabe);
-
-    // Return array with the result links
-    return $ausgabe;
-}
-
-/**
- * @param array $linksArray
- * array which contains internal and external links from the given url
- *
- * @param string $givenUrl
- * the given url
- *
- * @return array
- * associative array with sorted links (intern, extern)
- */
-function sortUrlList(array $linksArray, string $givenUrl) : array
-{
-    // Separate internal and external link and store it in the respective array
-    $internPattern = '/https?:\/\/'.$givenUrl.'/';
-    $internLinks = [];
-    $externLinks = [];
-
-    foreach ($linksArray[0] as $url)
-    {
-        if(preg_match_all($internPattern, $url) === 1)
-        {
-            array_push($internLinks, $url);
-        }
-        else
-        {
-            array_push($externLinks, $url);
-        }
-    }
-
-    $sortedlinks = [
-        'intern' => $internLinks,
-        'extern' => $externLinks
-    ];
-
-    return $sortedlinks;
-}
-
-function filterSortedUrlListAndSave(array $sortedUrlList, $domain)
-{
-    $filteredInternLinks = array_unique($sortedUrlList['intern']);
-    $filteredExternLinks = array_unique($sortedUrlList['extern']);
-
-    // Save the url's here
-    $domain->insertData($_POST, $filteredExternLinks, $filteredInternLinks);
-}
 
 // Set reload flag
 if(isset($_SESSION['lastSubmit']) && $_SESSION['lastSubmit'] == $_POST['domain'])
@@ -102,17 +36,17 @@ if(!empty($_POST['domain']) && !$reloaded)
     $domain->setDomain($_POST['domain']);
 
     // Start crawling
-    $linksArrayGlobal = searchWebLinkInUrl($domain->getDomain());
-    $sortedUrlList = sortUrlList($linksArrayGlobal, $domain->getDomain());
+    $linksArrayGlobal = $indexController->searchWebLinkInUrl($domain->getDomain());
+    $sortedUrlList = $indexController->sortUrlList($linksArrayGlobal, $domain->getDomain());
 
     // Filter $sortedUrlList and save them in db
-    filterSortedUrlListAndSave($sortedUrlList, $domain);
+    $indexController->filterSortedUrlListAndSave($sortedUrlList, $domain);
 
     var_dump($sortedUrlList);
-    echo '----------------------------- INTERNE LINKS';
+    echo '----------------------------- INTERN LINKS';
     var_dump($sortedUrlList['intern']);
 
-    echo '----------------------------- EXTERNE LINKS';
+    echo '----------------------------- EXTERN LINKS';
     var_dump($sortedUrlList['extern']);
 }
 
